64065f5a5379d9bbf491a8876a74919807760216
Richard Leung Woo Gabriel <leungwoo@gmail.com>
2022-04-10 17:39:18 -0400

    final prodcut

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'typingpractice';
+  public randomText = lorem.sentence();
+  public enteredText = '';
+
+  onInput(value: string) {
+    this.enteredText = value;
+  }
+  compare(randomLetter: string, enteredLetter: string) {
+    if (!enteredLetter) {
+      return 'pending';
+    }
+    return randomLetter === enteredLetter ? 'correct' : 'incorrect';
+  }
 }
